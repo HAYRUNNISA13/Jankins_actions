209404cef12fb0371380631fb29ac59e28c75c54
HAYRUNNısa13 <hayrun.celik@ogr.sakarya.edu.tr>
2025-05-21 13:36:06 +0000

    change1

--- src/main/java/com/example/demo/Demo4Application.java
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -15,6 +15,6 @@ public class Demo4Application {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot on Kubernetes!jenkinsss";
+        return "Hello from Spring Boot on Kubernetes!jenkinsssss";
     }
 }
