a1271ae73b48073a05588a9d04566500a077a0a7
HAYRUNNısa13 <hayrun.celik@ogr.sakarya.edu.tr>
2025-05-22 06:42:42 +0000

    change7

--- src/main/java/com/example/demo/Demo4Application.java
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -15,6 +15,6 @@ public class Demo4Application {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Kubernetessss+jjjjjjenkinsssssss";
+        return "Hello from Kubernetes+jjjjjjenkinsssssss";
     }
 }
